18fac31614d9c4d42a0044fc96bdc99204ed2a5f
mei-riko <dobrovolska.marina@gmail.com>
2021-09-11 14:25:37 +0300

    inputmask

--- src/items/input/input.js
+++ src/items/input/input.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 
 export let unitMaskPhone = () => {
     $('input[type="tel"]').inputmask({
-        mask: '+9 999 999 99 99',
+        mask: '+7 (999) 999 99 99',
         placeholder: " ",
         showMaskOnHover: false,
         onincomplete: function(){ 
@@ -68,13 +68,13 @@ $(function() {
         unitMaskPhone();
     }
     // Input Mask Name
-    if( $('input[name="name"]').length > 0 ) {
-        unitRegexName();
-    }
-    // Input Mask Email
-    if( $('input[name="mail"]').length > 0 ) {
-        unitRegexEmail();
-    }
+    // if( $('input[name="name"]').length > 0 ) {
+    //     unitRegexName();
+    // }
+    // // Input Mask Email
+    // if( $('input[name="mail"]').length > 0 ) {
+    //     unitRegexEmail();
+    // }
 });
 
 $('input').on('keydown', function(event) {
