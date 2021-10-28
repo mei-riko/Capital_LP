d803a22d6d3e960c27e6bb8bd77010751779f9dc
Hennadii Dobrovolskyi <hdobrovolskyi@envionsoftware.com>
2021-10-28 11:23:50 +0300

    fix ajaxloader

--- src/js/ajaxloader.js
+++ src/js/ajaxloader.js
@@ -1,15 +1,28 @@
 import $ from 'jquery';
 
 $(function() {
-    var initAjaxLoader = function(){
-        $('a[data-loader="ajax"]').click(function(e){
-            e.preventDefault();
-            let self = $(this);
-            let url = self.attr("href");
-            let target = self.data("target");
-            $(target).load(url, initAjaxLoader);
+    window.ajaxLoaderHandler = function(e){
+        e.preventDefault();
+        if(e.isPropagationStopped()) {
             return false;
-        });
+        }
+        e.stopPropagation()
+        let self = $(this);
+        let url = self.data("href");
+        let target = self.data("target");
+
+        $(target).load(url, initAjaxLoader);
+        return false;
+    };
+    var initAjaxLoader = function(){
+        $('[data-loader="ajax"]')
+        .off('click', window.ajaxLoaderHandler)
+        .on('click', window.ajaxLoaderHandler);
+
+        // Input Mask Phone
+        if( $('input[type="tel"]').length > 0 ) {
+            unitMaskPhone();
+        }
     };
     initAjaxLoader();
 });
